6fd4d6e0b495fd1581fc8c7be12ec26411adc6fc
Daniel Barna <barna.daniel@wigner.hu>
2024-12-13 14:20:21 +0100

    commit

--- c++/daq_settings.C
+++ c++/daq_settings.C
@@ -88,9 +88,9 @@ namespace apdcam10g
                 if(s == "mtu")
                 {
                     int m=0;
-//                    cerr<<"DEB1: "<<m<<endl;
+                    cerr<<"DEB1: "<<m<<endl;
                     cmd>>m;
-//                    cerr<<"DEB2: "<<m<<endl;
+                    cerr<<"DEB2: "<<m<<endl;
                     mtu(m); // Set MTU and calculate 'octet_'
                     mtu_ok = true;
                 }
